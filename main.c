746736596314ce20f07403419b93adaef4f9862b
King Jin <jinking.this@gmail.com>
2018-03-27 17:54:44 +0800

    update main func

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <apue.h>
 
 int main(int argc, char* argv[]) {
-    fprintf(stderr, "%s\n", "string");
+    if (lseek(STDERR_FILENO, 0, SEEK_CUR) == -1)
+        printf("can't seek\n");
+    else
+        printf("seek OK\n");
     exit(EXIT_SUCCESS);
 }
